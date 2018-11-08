5b9cae1249d64405e8ffdb548ead3e3b513aca83
Christian Nastasi <christian.nastasi@gmail.com>
2018-11-08 16:26:57 +0100

    feat(article): Add GetArticle Handler

---
+++ tests/Unit/Article/UseCase/GetArticle/GetArticleHandlerTest.php
@@ -0,0 +1,80 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Article\UseCase\GetArticle;
+
+use Acme\Academic\ValueObject\AcademicID;
+use Acme\Article\Article;
+use Acme\Article\Repository\ArticleRepository;
+use Acme\Article\Repository\Exception\ArticleNotFound;
+use Acme\Article\UseCase\CreateArticle\CreateArticleCommand;
+use Acme\Article\UseCase\CreateArticle\CreateArticleHandler;
+use Acme\Article\UseCase\GetArticle\GetArticleCommand;
+use Acme\Article\UseCase\GetArticle\GetArticleHandler;
+use Acme\Article\ValueObject\ArticleID;
+use Acme\Article\ValueObject\Body;
+use Acme\Article\ValueObject\Title;
+use Acme\Reviewer\ValueObject\ReviewerID;
+use PHPUnit\Framework\Assert;
+use Prophecy\Argument;
+use Tests\TestCase;
+
+/**
+ * @covers \Acme\Article\UseCase\CreateArticle\CreateArticleHandler
+ */
+final class GetArticleHandlerTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider articleDataProvider
+     */
+    public function should_create_an_article(Article $expectedArticle): void
+    {
+        $repositoryProphecy = $this->prophesize(ArticleRepository::class);
+
+        $repositoryProphecy->getById(Argument::type(ArticleID::class))
+                           ->shouldBeCalledOnce()
+                           ->willReturn($expectedArticle);
+
+        $repository = $repositoryProphecy->reveal();
+
+        $handler = new GetArticleHandler($repository);
+
+        $command = new GetArticleCommand($expectedArticle->id());
+
+        $article = $handler($command);
+
+        $this->assertSame($expectedArticle, $article);
+    }
+
+    /**
+     * @test
+     * @expectedException \Acme\Article\Repository\Exception\ArticleNotFound
+     */
+    public function should_not_found_an_article()
+    {
+        /** @var ArticleID $articleId */
+        $articleId = $this->factoryFaker->instance(ArticleID::class);
+
+        $repository = $this->prophesize(ArticleRepository::class);
+
+        $repository->getById($articleId)
+                   ->willThrow(new ArticleNotFound($articleId));
+
+
+        $handler = new GetArticleHandler($repository->reveal());
+
+        $handler(new GetArticleCommand($articleId));
+    }
+
+
+    public function articleDataProvider(): array
+    {
+        return [
+            [
+                $this->factoryFaker->instance(Article::class)
+            ],
+        ];
+    }
+}
