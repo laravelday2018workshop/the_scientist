9806245a9b0464a099ccebb3120d75835e6802e1
Christian Nastasi <christian.nastasi@gmail.com>
2018-11-18 16:34:10 +0100

    refactor(): Refactor AppService provider and improve test coverage

---
+++ app/Integration/Common/Query/CrudFacadeDefaultBuilder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Integration\Common\Query;
+
+use Illuminate\Database\DatabaseManager;
+
+final class CrudFacadeDefaultBuilder
+{
+    /**
+     * @var DatabaseManager
+     */
+    private $databaseManager;
+
+    public function __construct(DatabaseManager $databaseManager)
+    {
+        $this->databaseManager = $databaseManager;
+    }
+
+    public function build(string $table): CrudFacadeDefault
+    {
+        $builder = $this->databaseManager->table($table);
+
+        return new CrudFacadeDefault(
+            new QueryBuilderSelectById($builder),
+            new QueryBuilderSelectAll($builder),
+            new QueryBuilderInsert($builder),
+            new QueryBuilderUpdate($builder),
+            new QueryBuilderDelete($builder)
+        );
+    }
+}
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,69 +6,46 @@ namespace App\Providers;
 
 use Acme\Academic\Repository\AcademicRepository;
 use Acme\Article\Repository\ArticleRepository;
+use App\Integration\Academic\Mapper\AcademicMapper;
 use App\Integration\Academic\Mapper\DatabaseAcademicMapper;
-use App\Integration\Academic\Mapper\FromAcademicPartialMapping;
 use App\Integration\Academic\Repository\AcademicQueryBuilderRepository;
+use App\Integration\Article\Mapper\ArticleMapper;
 use App\Integration\Article\Mapper\DatabaseArticleMapper;
-use App\Integration\Article\Mapper\FromArticlePartialMapping;
 use App\Integration\Article\Repository\ArticleQueryBuilderRepository;
-use App\Integration\Common\Query\CrudFacadeDefault;
-use App\Integration\Common\Query\QueryBuilderDelete;
-use App\Integration\Common\Query\QueryBuilderInsert;
-use App\Integration\Common\Query\QueryBuilderSelectAll;
-use App\Integration\Common\Query\QueryBuilderSelectById;
-use App\Integration\Common\Query\QueryBuilderUpdate;
-use Illuminate\Database\DatabaseManager;
-use Illuminate\Log\Logger;
+use App\Integration\Common\Query\CrudFacade;
+use App\Integration\Common\Query\CrudFacadeDefaultBuilder;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
-    {
-    }
-
-    /**
-     * Register any application services.
-     */
     public function register()
     {
-        $this->app->bind(ArticleRepository::class,
-            function (): ArticleQueryBuilderRepository {
-                return new ArticleQueryBuilderRepository(
-                    $this->app->get('db'),
-                    new DatabaseArticleMapper(new FromArticlePartialMapping()),
-                    $this->app->get(Logger::class)
-                );
-            });
-
-        $this->app->bind(AcademicRepository::class,
-            function (): AcademicQueryBuilderRepository {
-                /** @var DatabaseManager $databaseManager */
-                $databaseManager = $this->app->get('db');
-
-                return new AcademicQueryBuilderRepository(
-                    $databaseManager,
-                    $this->buildCrudFacade($databaseManager, AcademicQueryBuilderRepository::TABLE_NAME),
-                    new DatabaseAcademicMapper(new FromAcademicPartialMapping()),
-                    $this->app->get(Logger::class)
-                );
-            });
+        $this->registerArticleRepository();
+        $this->registerAcademicRepository();
     }
 
-    private function buildCrudFacade(DatabaseManager $databaseManager, string $table)
+    private function registerArticleRepository(): void
     {
-        $builder = $databaseManager->table($table);
+        $this->app->bind(ArticleRepository::class, ArticleQueryBuilderRepository::class);
 
-        return new CrudFacadeDefault(
-            new QueryBuilderSelectById($builder),
-            new QueryBuilderSelectAll($builder),
-            new QueryBuilderInsert($builder),
-            new QueryBuilderUpdate($builder),
-            new QueryBuilderDelete($builder)
-        );
+        $this->app->when(ArticleRepository::class)
+                  ->needs(ArticleMapper::class)
+                  ->give(DatabaseArticleMapper::class);
+    }
+
+    private function registerAcademicRepository(): void
+    {
+        $this->app->bind(AcademicRepository::class, AcademicQueryBuilderRepository::class);
+
+        $this->app->when(AcademicRepository::class)
+                  ->needs(AcademicMapper::class)
+                  ->give(DatabaseAcademicMapper::class);
+
+        $this->app->when(AcademicRepository::class)
+                  ->needs(CrudFacade::class)
+                  ->give(function () {
+                      return $this->app->get(CrudFacadeDefaultBuilder::class)
+                                       ->build(AcademicQueryBuilderRepository::TABLE_NAME);
+                  });
     }
 }
