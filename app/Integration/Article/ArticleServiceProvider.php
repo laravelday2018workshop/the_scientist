2501270eaaf9bf1413800591c91b6c1695276f9f
Christian Nastasi <christian.nastasi@gmail.com>
2018-11-26 13:09:36 +0100

    refactor: Refactor AppServiceProvider

---
+++ app/Integration/Article/ArticleServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Integration\Article;
+
+use Acme\Article\Repository\ArticleRepository;
+use App\Integration\Article\Mapper\Hydrator\DefaultHydrateArticle;
+use App\Integration\Article\Mapper\Hydrator\HydrateArticle;
+use App\Integration\Article\Mapper\Serializer\DefaultSerializeArticle;
+use App\Integration\Article\Mapper\Serializer\SerializeArticle;
+use App\Integration\Article\Repository\ArticleQueryBuilderRepository;
+use App\Integration\Common\Query\CrudFacade;
+use App\Integration\Common\Query\CrudFacadeDefaultBuilder;
+use Illuminate\Support\ServiceProvider;
+
+class ArticleServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap services.
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register services.
+     */
+    public function register()
+    {
+        $this->app->bind(ArticleRepository::class, ArticleQueryBuilderRepository::class);
+        $this->app->when(ArticleRepository::class)->needs(CrudFacade::class)->give(function () {
+            return $this->app->get(CrudFacadeDefaultBuilder::class)->build(ArticleQueryBuilderRepository::TABLE_NAME);
+        });
+
+        $this->app->bind(SerializeArticle::class, DefaultSerializeArticle::class);
+        $this->app->bind(HydrateArticle::class, DefaultHydrateArticle::class);
+    }
+}
