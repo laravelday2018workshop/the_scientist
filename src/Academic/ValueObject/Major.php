e9a1e0cae1c501e7c1c8a59d421e1d15e319fa3f
Damiano Petrungaro <damianopetrungaro@gmail.com>
2018-11-26 18:30:08 +0100

    feat: Decorate Major with a custom error

--- src/Academic/ValueObject/Major.php
+++ src/Academic/ValueObject/Major.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 
 namespace Acme\Academic\ValueObject;
 
+use Acme\Academic\ValueObject\Exception\InvalidMajor;
 use MyCLabs\Enum\Enum;
+use UnexpectedValueException;
 
 /**
  * @method static self BIOLOGICAL_SCIENCE()
@@ -15,4 +17,13 @@ final class Major extends Enum
     private const BIOLOGICAL_SCIENCE = 'biological_science';
 
     private const COMPUTER_SCIENCE = 'computer_science';
+
+    public function __construct($value)
+    {
+        try {
+            parent::__construct($value);
+        } catch (UnexpectedValueException $e) {
+            throw new InvalidMajor($value, $e);
+        }
+    }
 }
