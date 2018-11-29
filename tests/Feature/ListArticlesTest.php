c3b866698f6cda1d2e129329461be5ed4db1fbb6
mariafanelli-ym <maria.fanelli@youniversal.com>
2018-11-29 14:47:18 +0100

    feat(article):Add ListArticlesController

---
+++ tests/Feature/ListArticlesTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class ListArticlesTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @test
+     */
+    public function shouldListArticles()  // oppure testListArticles
+    {
+        $response = $this->get('api/articles');
+        $response->assertStatus(200);
+        $response->assertJson([]);
+    }
+}
